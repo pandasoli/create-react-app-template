d72b3d154ff8442fa7617373e931a3c39121ad46
pandasoli <sadpandasoli@gmail.com>
2022-06-23 23:25:56 -0300

    [update] move all styles to styles/ and update rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,20 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
-import { Container, Contents } from './index.styles'
+import { Container, Contents } from './styles/index.styles'
 import { GlobalStyle } from './styles/globals'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+const container = document.getElementById('root') as HTMLElement
+const root = createRoot(container)
 
-ReactDOM.render(
+root.render(
   <Container>
     <Contents>
       <App/>
     </Contents>
 
     <GlobalStyle/>
-  </Container>,
-  document.getElementById('root')
+  </Container>
 )
