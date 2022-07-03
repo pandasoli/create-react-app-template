37e0d0240e0d35a4d7a8647187806414717fe99a
pandasoli <sadpandasoli@gmail.com>
2022-07-02 22:11:46 -0300

    [add] Test components route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/home'
+import Test from './pages/test'
 import Error from './pages/error'
 
 
@@ -8,6 +9,7 @@ const App = () =>
   <BrowserRouter>
     <Routes>
       <Route path='/' element={ <Home/> }/>
+      <Route path='/test' element={ <Test/> }/>
       <Route path='/error' element={ <Error/> }/>
     </Routes>
   </BrowserRouter>
