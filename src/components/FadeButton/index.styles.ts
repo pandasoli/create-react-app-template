6c6f18a2722c43a5c78d87970bff27fff935957e
pandasoli <sadpandasoli@gmail.com>
2022-07-02 22:04:43 -0300

    [add] FadeButton

---
+++ src/components/FadeButton/index.styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+
+export default styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+
+  padding: 4px 60px;
+
+  cursor: pointer;
+`
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+`
---
+++ src/components/FadeButton/index.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, useState } from 'react'
+
+import Button from '../Button.styles'
+import Container, { Header, Main } from './index.styles'
+
+type Props = {
+  text: string
+  children: ReactNode
+}
+
+
+const FadeButton = (props: Props) => {
+  const [ Open, SetOpen ] = useState(false)
+
+  return <Container>
+    <Header>
+      <Button color='primary' outline onClick={ () => SetOpen(!Open) }>{ props.text }</Button>
+    </Header>
+    <Main>
+      { Open && props.children }
+    </Main>
+  </Container>
+}
+
+
+export default FadeButton
