c1c5b2cdac8db464321945dbb80cd060300eb188
pandasoli <sadpandasoli@gmail.com>
2022-07-02 22:11:32 -0300

    [add] Test components route

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 
 import App from './App'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 import { Container, Contents } from './styles/index.styles'
 import { GlobalStyle } from './styles/globals'
@@ -11,10 +13,13 @@ const root = createRoot(container)
 
 root.render(
   <Container>
+    <Header/>
+
     <Contents>
       <App/>
     </Contents>
 
+    <Footer/>
     <GlobalStyle/>
   </Container>
 )
