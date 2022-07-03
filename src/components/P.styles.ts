d2a66f5e279fac3ae3e98b7c6b8e97be600f2bea
pandasoli <sadpandasoli@gmail.com>
2022-07-02 22:06:38 -0300

    [add] Reactstrap paragraph restyled

---
+++ src/components/P.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+
+export default styled.p`
+  margin: 10px;
+
+  text-align: center;
+
+  @media (min-width: 768px) { max-width: 768px; } // --sm-size
+  @media (min-width: 992px) { max-width: 992px; } // --md-size
+`
