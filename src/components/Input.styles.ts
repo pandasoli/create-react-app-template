0e2e3fc6711905bbdb92160a9f207cbf9b6702da
Panda Soli <sadpandasoli@gmail.com>
2022-07-04 21:27:12 -0300

    [added] error colors

--- src/components/Input.styles.ts
+++ src/components/Input.styles.ts
@@ -4,15 +4,28 @@ import { Input } from 'reactstrap'
 
 
 export default styled(Input)`
+  transition: background-color .6s, color .6s, border-color .6s, box-shadow .2s;
+
   &[type='text'],
+  &[type='password'],
   &[type='number'] {
-    transition: color .2s;
-    border-color: var(--primary-cl-light);
-    background-color: var(--primary-cl-light);
+    --cl: var(--secondary-cl);
+    --cl-rgb: var(--secondary-cl-rgb);
+
+    color: var(--cl) !important;
+    border-color: var(--cl) !important;
+
+    ::selection {
+      color: var(--primary-cl);
+      background-color: var(--cl);
+    }
+
+    &.success { --cl: var(--green-cl); --cl-rgb: var(--green-cl-rgb); }
+    &.warning { --cl: var(--yellow-cl); --cl-rgb: var(--yellow-cl-rgb); }
+    &.error { --cl: var(--red-cl); --cl-rgb: var(--red-cl-rgb); }
 
     &:focus {
-      color: var(--primary-font-cl);
-      box-shadow: 0 0 0 .25rem rgb(var(--secondary-cl-rgb), .68);
+      box-shadow: 0 0 0 .25rem rgb(var(--cl-rgb), .68);
     }
   }
 
